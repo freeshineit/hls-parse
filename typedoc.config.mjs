f443502f36791ca7b33d21dbd260d1db68fe2e82
ShineShao <xiaoshaoqq@gmail.com>
2026-08-09 00:35:36 +0800

    feat: add zh-CN docs

--- typedoc.config.mjs
+++ typedoc.config.mjs
@@ -1,17 +1,18 @@
 const formatDate = `${new Date().getFullYear()}-${String(new Date().getMonth() + 1).padStart(2, "0")}-${String(new Date().getDate()).padStart(2, "0")}`;
 
+const isZh = process.env.DOC_LANG === "zh";
+
 export default {
   entryPoints: ["src/*.ts"],
-  // 这里设置头部标题
-  name: "API Docs",
-  out: "docs",
+  name: isZh ? "API 文档" : "API Docs",
+  out: isZh ? "docs/zh" : "docs",
   plugin: ["typedoc-plugin-rename-defaults", "typedoc-plugin-mdn-links", "typedoc-plugin-replace-text"],
   exclude: ["node_modules", "__tests__/**/*", "packages/*/{__tests__,e2e}/**/*"],
-  // includeVersion: true,
   hideGenerator: true,
   disableSources: false,
+  includeVersion: true,
   tsconfig: "tsconfig.json",
-  readme: "README.md",
+  readme: isZh ? "README.zh-CN.md" : "README.md",
   githubPages: true,
   gitRemote: "origin",
   highlightLanguages: ["typescript", "javascript", "css", "html", "json", "scss", "jsx", "tsx", "bash"],
@@ -27,8 +28,17 @@ export default {
     ],
   },
   navigationLinks: {
-    Examples: "https://github.com/freeshineit/hls-parse/tree/main/examples",
-    Github: "https://github.com/freeshineit/hls-parse",
+    ...(isZh
+      ? {
+          示例: "https://github.com/freeshineit/hls-parse/tree/main/examples",
+          "English Docs": "../",
+          Github: "https://github.com/freeshineit/hls-parse",
+        }
+      : {
+          Examples: "https://github.com/freeshineit/hls-parse/tree/main/examples",
+          中文文档: "./zh/",
+          Github: "https://github.com/freeshineit/hls-parse",
+        }),
   },
   customFooterHtml: `<p style="text-align: center;">Copyright © ${formatDate} <a href="https://github.com/freeshineit" target="_blank">ShineShao</a></p>`,
 };
